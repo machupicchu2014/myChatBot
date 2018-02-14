7be0c7ab8a340d23561156cb8eb964690eb932f8
Matthew Cox <machupicchu2014@gmail.com>
2018-02-13 23:00:12 -0700

    First commit

---
+++ script.js
@@ -0,0 +1,38 @@
+var cs = "";
+var conversation = "";
+
+$("#document").ready(function(){
+  $("#inputSubmit").click(function(e){
+    e.preventDefault();
+    console.log($("#in").val());
+    conversation += '<p class="user">' + $("#in").val() + '</p>';
+    $("#conversation").html(conversation);
+    var myURL;
+    if(cs === ""){
+      myURL = "http://www.cleverbot.com/getreply?key=CC76znb3bo6rlz-GvH2FHrbWbDA&input=" + $("#in").val();
+    } else {
+      myURL = "http://www.cleverbot.com/getreply?key=CC76znb3bo6rlz-GvH2FHrbWbDA&input=" + $("#in").val() + "&cs=" + cs;
+    }
+    $("#in").val("");
+    $(window).scrollTop($(document).height());
+    $.ajax({
+      url : myURL,
+      dataType : "json",
+      success : function(json) {
+        console.log(json);
+        cs = json.cs;
+        conversation += '<p class="bot">' + json.clever_output + '</p>';
+        $("#conversation").html(conversation);
+        $(window).scrollTop($(document).height());
+	    }
+
+	});
+
+
+
+  })
+
+
+
+
+})
